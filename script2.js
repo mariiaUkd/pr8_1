cbd7ca8dc35ba77d32b4b4cbfa8bb7945c5ce3fa
mariiaUkd <168064430+mariiaUkd@users.noreply.github.com>
2026-04-27 14:22:44 +0300

    task2

---
+++ script2.js
@@ -0,0 +1,7 @@
+function showUserInfo(name, age, city, country) {
+    
+    const message = `Користувач: ${name}, вік: ${age} років, проживає в місті ${city} (${country}).`;
+    console.log(message);
+}
+
+showUserInfo("Марія", 18, "Івано-Франківськ", "Україна");
